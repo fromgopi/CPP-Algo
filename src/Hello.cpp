ad58b99a96e7eaf468487d68fabe7bd8bf5e014c
Gopi Krishna M <gopt.muktevi@gmail.com>
2021-07-10 09:13:23 +0530

    Making changes in Algo

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-int main() {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"} ;
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg) {
+    for (const string& word : msg)
+    {
         cout << word << " ";
     }
     cout << endl;
